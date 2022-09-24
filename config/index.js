7e3e776ec4a6895513eafbdb0cbc69717a0eefdf
Wayne <2268348563@qq.com>
2022-09-24 23:09:52 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -67,7 +67,7 @@ const USER_CONFIG = {
         // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
         id: 'oTpBu5_nITreNph7CH0yBuzejlb4',
         // 你想对他发送的模板消息的模板ID
-        useTemplateId: 'BcZy9Y-222mWExDkxsIPgrtVhQV_nnePv3hom0uzcSk',
+        useTemplateId: 'pH9Bm0MiR7ljkzGYXRgje4Z9-wmR_Inh5HZMmoNtkQ4',
         // 所在省份
         province: '上海',
         // 所在城市
