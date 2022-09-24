a4d16e017f389a5fa6aea5d1df557a4b228b04b7
Wayne <2268348563@qq.com>
2022-09-24 22:20:28 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -45,7 +45,7 @@ const USER_CONFIG = {
     horoscope: false,
 
     /** 生日消息和节日消息 */
-    birthdayMessage: true,
+    birthdayMessage: false,
 
     // 学生课表
     courseSchedule: false,
@@ -63,15 +63,15 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '老婆0',
+      name: '老婆',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oTpBu5xcn1jANdgbaUz5tgVTMD7E',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'HPBTcXsKzJeZaNZASJ-ovPZwYcDIyv4WxS2dwRkorpA',
       // 所在省份
-      province: '广东',
+      province: '上海',
       // 所在城市
-      city: '肇庆',
+      city: '上海',
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '12-27',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
@@ -98,9 +98,9 @@ const USER_CONFIG = {
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2015-11-10' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '2021-12-25' },
         // 退伍日
         { keyword: 'ex_day', date: '2022-09-10' }
       ],
